be5dab4b52c8b584c8961ace12d66e92a0023e30
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-11-05 16:14:25 +0300

    added screenshot widget

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Screen to pdf in flutter
 
+Packages used:
+
+pdf: ^3.8.4 // to create a pdf
+screenshot: ^1.2.3 // to take a screenshot
+pdfx: ^2.3.0 // to view pdf
+share_plus: ^6.2.0 //for file sharing
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,15 +1,25 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:screenshot/screenshot.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  HomePage({Key? key}) : super(key: key);
+  Uint8List? _imageFile;
+  ScreenshotController screenshotController = ScreenshotController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Screen To Pdf"),),
-      body:const Center(
-        child: Text("Pdf Ekranı"),
-      ) ,
+      appBar: AppBar(
+        title: const Text("Screen To Pdf"),
+      ),
+      body: Screenshot(
+        controller: screenshotController,
+        child: const Center(
+          child: Text("Pdf Ekranı"),
+        ),
+      ),
     );
   }
 }
